3db91f0776a5abeb1bec00d9ebfa1ae5c79acd77
Ian Norris <fork@iostream.uk>
2024-04-23 01:27:16 +0100

    Moved regs to the bottom

--- EnkelCrashDecoder.Client/wwwroot/layout.js
+++ EnkelCrashDecoder.Client/wwwroot/layout.js
@@ -1,50 +1,62 @@
 
 export class LayoutManager {
 
-    static init(sessionId) {
+	static init(sessionId) {
 
-        var config = {
-            content: [{
-                type: 'row',
-                content: [{
-                    type: 'component',
-                    componentName: 'text',
-                    componentState: { sessionId: sessionId }
-                }, {
-                    type: 'column',
-                    content: [{
-                        type: 'component',
-                        componentName: 'regs',
-                        componentState: { sessionId: sessionId }
-                    }, {
-                        type: 'component',
-                        componentName: 'text',
-                        componentState: { sessionId: sessionId }
-                    }]
-                }]
-            }]
-        };
+		var config = {
+			content: [{
+				type: 'column',
+				content: [
+					{
+						type: 'row',
+						height: '80',
+						content:
+							[
+								{
+									type: 'component',
+									componentName: 'text',
+									componentState: { sessionId: sessionId }
+								},
+								{
+									type: 'component',
+									componentName: 'text',
+									componentState: { sessionId: sessionId }
+								}
+							]
+					},
+					{
+						type: 'row',
+						content: [{
+							type: 'component',
+							componentName: 'regs',
+							height: '20',
+							componentState: { sessionId: sessionId }
+						}]
+					}
+				]
+			}]
+		};
 
-        var buildComponent = function (container, componentState) {
-            var futureObject = Blazor.rootComponents.add(container._contentElement[0], componentState.componentName, componentState);
-            
-            futureObject.then(component => {
-                componentState.component = component;
-            });
-        };
+		var buildComponent = function (container, componentState) {
+			var futureObject = Blazor.rootComponents.add(container._contentElement[0], componentState.componentName, componentState);
 
-        var gl = new GoldenLayout(config, '#layout-root');
+			futureObject.then(component => {
+				componentState.component = component;
+			});
+		};
 
-        gl.registerComponent('regs', buildComponent);
-        gl.registerComponent('text', buildComponent);
+		var gl = new GoldenLayout(config, '#layout-root');
 
-        window.gl = gl;
+		gl.registerComponent('regs', buildComponent);
+		gl.registerComponent('text', buildComponent);
 
-        gl.init();
+		window.gl = gl;
 
-        $(window).resize(function () {
-            let area = $('#layout-root');
-            gl.updateSize(area.width(), area.height());
-        });
-    }
+		gl.init();
+
+		$(window).resize(function () {
+			let area = $('#layout-root');
+			gl.updateSize(area.width(), area.height());
+		});
+	}
 }
